70c4107166740eea649a5e1537f77b87ee5efec8
tanxiaocong <2268740858@qq.com>
2020-01-06 01:51:05 +0800

    ‘1-06’

--- src/main/java/com/p2p/controller/BorrowController.java
+++ src/main/java/com/p2p/controller/BorrowController.java
@@ -1,15 +1,19 @@
 package com.p2p.controller;
 
+import com.github.pagehelper.Page;
+import com.p2p.dto.DataDto;
+import com.p2p.model.AccountFlow;
 import com.p2p.model.Borrow;
-import com.p2p.model.Repayment;
+import com.p2p.service.IAccountFlowService;
 import com.p2p.service.IBorrowService;
-import com.p2p.service.IRepaymentService;
+import com.p2p.util.PageBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,7 +26,7 @@ public class BorrowController {
     private IBorrowService borrowService;
 
     @Autowired
-    private IRepaymentService repaymentService;
+    private IAccountFlowService iAccountFlowService;
 
     @RequestMapping(value = "/addBorrow")
     @ResponseBody
@@ -40,19 +44,74 @@ public class BorrowController {
 
     @RequestMapping(value = "/QueryBorrow")
     @ResponseBody
-    public Map QueryBorrow(Borrow borrow){
-        Map<String,Object> json = new HashMap<String,Object>();
+    public List QueryBorrow(Borrow borrow){
         List<Borrow> borrows = borrowService.QueryListBorrow(borrow);
-        json.put("borrows",borrows);
-        Repayment repayment = null;
-        for (Borrow borrow1 : borrows) {
-            repayment = new Repayment();
-            Integer borrowId = borrow1.getBorrowId();
-            repayment.setBorrowid(borrowId);
-            List<Repayment> repayments = repaymentService.RepaymentQuerySing(repayment);
-            json.put("repayments",repayments);
-        }
-        return json;
+        return borrows;
     }
+
+
+    @RequestMapping(value = "/listBorrowPage")
+    @ResponseBody
+    public Map listBorrowPage(Borrow borrow, DataDto dataDto,HttpServletRequest req){
+        Map map =new HashMap();
+        PageBean pageBean = new PageBean();
+        pageBean.initPageBean(req,pageBean);
+        List<Borrow> borrows = borrowService.listPage(borrow.getUserName(), dataDto.getStartDate(), dataDto.getEndDate(), pageBean);
+
+        map.put("borrows", borrows);
+        map.put("page", pageBean.getPage());
+        map.put("total", pageBean.getTotal());
+        map.put("rows", pageBean.getRows());
+
+        return map;
+    }
+
+
+    @RequestMapping(value = "/listStatusPage")
+    @ResponseBody
+    public Map listStatusPage(Borrow borrow, DataDto dataDto,HttpServletRequest req){
+        System.out.println("-----------------"+borrow.getBorrowStatus());
+        Map map =new HashMap();
+        PageBean pageBean = new PageBean();
+        pageBean.initPageBean(req,pageBean);
+        List<Borrow> borrows = borrowService.listStatusPage(borrow.getUserName(),dataDto.getStartDate(),dataDto.getEndDate(),pageBean);
+
+        map.put("borrows", borrows);
+        map.put("page", pageBean.getPage());
+        map.put("total", pageBean.getTotal());
+        map.put("rows", pageBean.getRows());
+
+        return map;
+    }
+
+
+    @RequestMapping(value = "/updateStatus")
+    @ResponseBody
+    public Map updateStatus(Borrow borrow){
+        System.out.println("--------------------------"+borrow);
+        Map map =new HashMap();
+        int i = borrowService.updateStatus(borrow);
+        map.put("code",500);
+        if(i>0){
+            map.put("code",200);
+
+            //成功的同时加一条系统流水
+            AccountFlow accountFlow = new AccountFlow();
+            accountFlow.setUserName(borrow.getUserName());
+            accountFlow.setFlowMoney(borrow.getBorrowMoney());
+            accountFlow.setFlowBorrow(borrow.getBorrowTitle());
+            accountFlow.setFlowType("借款");
+            int a = iAccountFlowService.insertSelective(accountFlow);
+            System.out.println("================"+a);
+        }
+
+        return map;
+    }
+
+
+
+
+
+
 
 }
