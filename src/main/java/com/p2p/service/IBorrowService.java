8ab9b712834f594bd157eb2b86ce9290f7e38116
tanxiaocong <2268740858@qq.com>
2020-01-06 00:46:31 +0800

    ‘1-06’

--- src/main/java/com/p2p/service/IBorrowService.java
+++ src/main/java/com/p2p/service/IBorrowService.java
@@ -1,6 +1,9 @@
 package com.p2p.service;
 
+import com.p2p.dto.DataDto;
 import com.p2p.model.Borrow;
+import com.p2p.util.PageBean;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -18,4 +21,18 @@ public interface IBorrowService {
     int updateByPrimaryKey(Borrow record);
 
     List<Borrow> QueryListBorrow(Borrow record);
+
+    //模查时间，姓名
+    List<Borrow> listPage(String userName,String startDate , String endDate , PageBean pageBean);
+
+    //查询状态为0的数据
+    List<Borrow> listStatusPage(String userName,String startDate , String endDate , PageBean pageBean);
+
+    //修改状态为1
+    int updateStatus(Borrow borrow);
+
+
+
+
+
 }
