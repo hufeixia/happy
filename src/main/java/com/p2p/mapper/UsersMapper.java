c100ebd77dd8d8fe73edcc097136c0a880d1b323
tanxiaocong <2268740858@qq.com>
2020-01-06 00:43:36 +0800

    ‘1-06’

--- src/main/java/com/p2p/mapper/UsersMapper.java
+++ src/main/java/com/p2p/mapper/UsersMapper.java
@@ -1,7 +1,13 @@
 package com.p2p.mapper;
 
+import com.p2p.dto.UserDto;
 import com.p2p.model.Users;
+import com.p2p.util.PageBean;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface UsersMapper {
@@ -18,5 +24,8 @@ public interface UsersMapper {
     int updateByPrimaryKey(Users record);
 
     Users UsersByPrimaryName(String userName);
+
+    //连表查询
+    List<UserDto> seleDtoPage(@Param("username")String username,@Param("startDate")String startDate,@Param("endDate")String endDate, PageBean pageBean);
 
 }
